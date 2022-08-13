253091139613092aab98b6c432207344cda7a67c
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-13 12:15:07 +0800

    Update ChinaASN.py

--- action/ChinaASN.py
+++ action/ChinaASN.py
@@ -1,8 +1,8 @@
 import requests
 
-rawAppleDomain = requests.get("https://ruleset.skk.moe/List/domainset/apple_cdn.conf").text
+rawChinaASN = requests.get("https://raw.githubusercontent.com/VirgilClyne/VirgilClyne/main/modules/ASN/ASN.list").text
 
-result = rawAppleDomain.split("\n")
+result = rawChinaASN.split("\n")
 
-with open("./AppleDomain.conf", "w") as f:
+with open("./ChinaASN.conf", "w") as f:
     f.write("\n".join(result))
