b5701f56ffbb87d1cc5326bb8d352eb56e6e177c
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-13 11:48:33 +0800

    Create GlobalMedia.py

---
+++ action/GlobalMedia.py
@@ -0,0 +1,13 @@
+import requests
+
+rawNetflix = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Netflix/Netflix.list").text
+rawDisney = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Disney/Disney.list").text
+rawYouTube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/YouTube/YouTube.list").text
+rawTwitch = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Twitch/Twitch.list").text
+
+
+
+result = rawNetflix.split("\n") + rawDisney.split("\n") + rawYouTube.split("\n") + rawTwitch.split("\n")
+
+with open("./GlobalMedia.conf", "w") as f:
+    f.write("\n".join(result))
