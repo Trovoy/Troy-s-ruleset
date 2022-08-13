bfde01c1d1cc0df66462911f3a9bcc390fdf133e
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-13 12:26:51 +0800

    Update GlobalMedia.py

--- action/GlobalMedia.py
+++ action/GlobalMedia.py
@@ -4,10 +4,11 @@ rawNetflix = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_ru
 rawDisney = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Disney/Disney.list").text
 rawYouTube = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/YouTube/YouTube.list").text
 rawTwitch = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Twitch/Twitch.list").text
+rawEmby = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Emby/Emby.list").text
 
 
 
-result = rawNetflix.split("\n") + rawDisney.split("\n") + rawYouTube.split("\n") + rawTwitch.split("\n")
+result = rawNetflix.split("\n") + rawDisney.split("\n") + rawYouTube.split("\n") + rawTwitch.split("\n") + rawEmby.split("\n")
 
 with open("./GlobalMedia.conf", "w") as f:
     f.write("\n".join(result))
