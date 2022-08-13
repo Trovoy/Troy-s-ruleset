d4c185e087fdeda09e061f50123d4fb016240e03
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-13 21:54:25 +0800

    Delete AppleDomain.py

--- action/AppleDomain.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-rawAppleDomain = requests.get("https://ruleset.skk.moe/List/domainset/apple_cdn.conf").text
-
-result = rawAppleDomain.split("\n")
-
-with open("./AppleDomain.conf", "w") as f:
-    f.write("\n".join(result))
