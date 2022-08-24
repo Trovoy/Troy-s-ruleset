e6294928038abaeaf73a1c239b6765442fe59df9
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-24 21:11:56 +0800

    Update RejectDomain.py

--- action/RejectDomain.py
+++ action/RejectDomain.py
@@ -1,6 +1,6 @@
 import requests
 
-rawRejectDomain = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Advertising/Advertising_Domain.list").text
+rawRejectDomain = requests.get("https://ruleset.skk.moe/List/domainset/reject.conf").text
 
 result = rawRejectDomain.split("\n")
 
