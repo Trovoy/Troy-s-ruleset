43a78c0c101bfd78a13a04b36d8c0980a9501641
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-30 11:14:46 +0800

    Update RejectDomain.py

--- action/RejectDomain.py
+++ action/RejectDomain.py
@@ -2,7 +2,10 @@ import requests
 
 rawRejectDomain = requests.get("https://ruleset.skk.moe/List/domainset/reject.conf").text
 
-result = rawRejectDomain.split("\n")
+result = list()
+for rawresult in [rawRejectDomain]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./RejectDomain.conf", "w") as f:
     f.write("\n".join(result))
