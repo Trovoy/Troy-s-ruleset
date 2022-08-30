8cd26efa4d7eab860ba9cbdff0e2beb3ed04dd54
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-30 11:14:21 +0800

    Update Reject.py

--- action/Reject.py
+++ action/Reject.py
@@ -3,7 +3,10 @@ import requests
 rawnonip = requests.get("https://ruleset.skk.moe/List/non_ip/reject.conf").text
 rawip = requests.get("https://ruleset.skk.moe/List/ip/reject.conf").text
 
-result = rawnonip.split("\n") + rawip.split("\n")
+result = list()
+for rawresult in [rawnonip , rawip]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./Reject.conf", "w") as f:
     f.write("\n".join(result))
