2fe6879ad0722c54faaeb0eb2c35b0e4b17577f1
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-30 10:44:53 +0800

    Update Global.py

--- action/Global.py
+++ action/Global.py
@@ -2,7 +2,10 @@ import requests
 
 rawGlobal = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Global/Global.list").text
 
-result = rawGlobal.split("\n")
+result = list()
+for rawresult in [rawGlobal]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./Global.conf", "w") as f:
     f.write("\n".join(result))
