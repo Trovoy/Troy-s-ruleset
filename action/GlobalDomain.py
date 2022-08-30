d6993f77adcb9636a69f88e3fe4f60188f6b4edf
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-30 11:12:19 +0800

    Update GlobalDomain.py

--- action/GlobalDomain.py
+++ action/GlobalDomain.py
@@ -2,7 +2,10 @@ import requests
 
 rawGlobalDomian = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Global/Global_Domain.list").text
 
-result = rawGlobalDomian.split("\n")
+result = list()
+for rawresult in [rawGlobalDomian]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./GlobalDomain.conf", "w") as f:
     f.write("\n".join(result))
