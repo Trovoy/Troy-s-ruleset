43ca1d68008d5f9fd3518c30aaa9af13e4f2b3ed
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-30 11:10:10 +0800

    Update AsianMedia.py

--- action/AsianMedia.py
+++ action/AsianMedia.py
@@ -3,7 +3,10 @@ import requests
 rawBiliBili = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/BiliBili/BiliBili.list").text
 rawBiliBiliIntl = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/BiliBiliIntl/BiliBiliIntl.list").text
 
-result = rawBiliBili.split("\n") + rawBiliBiliIntl.split("\n")
+result = list()
+for rawresult in [rawBiliBili, rawBiliBiliIntl]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./AsianMedia.conf", "w") as f:
     f.write("\n".join(result))
