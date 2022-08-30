a22905806603982acbe8dd9688c62a82ba8db814
Troy <105072381+Trovoy@users.noreply.github.com>
2022-08-30 11:11:00 +0800

    Update ChinaASN.py

--- action/ChinaASN.py
+++ action/ChinaASN.py
@@ -2,7 +2,10 @@ import requests
 
 rawChinaASN = requests.get("https://raw.githubusercontent.com/VirgilClyne/VirgilClyne/main/modules/ASN/ASN.list").text
 
-result = rawChinaASN.split("\n")
+result = list()
+for rawresult in [rawChinaASN]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
 
 with open("./ChinaASN.conf", "w") as f:
     f.write("\n".join(result))
