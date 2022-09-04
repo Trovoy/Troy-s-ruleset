afe7dbddc5c5a9ba4d9fe237b92144ed9eebdc4c
Troy <105072381+Trovoy@users.noreply.github.com>
2022-09-04 14:59:20 +0800

    Update Direct.py

--- action/Direct.py
+++ action/Direct.py
@@ -1,10 +1,11 @@
 import requests
 
 rawApple = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Apple/Apple.list").text
-rawWeChat = requests.get("https://raw.githubusercontent.com/NobyDa/Script/master/Surge/WeChat.list").text
+rawWeChat = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/WeChat/WeChat.list").text
+rawDirect = requests.get("https://gist.githubusercontent.com/Trovoy/2b18881892479e0c20386712d75767fc/raw/Direct.list").text
 
 result = list()
-for rawresult in [rawApple , rawWeChat]:
+for rawresult in [rawApple , rawWeChat , rawDirect]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 
