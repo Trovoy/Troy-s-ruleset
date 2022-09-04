28f5632e41859df1487e7908e55cf83f385b17be
Troy <105072381+Trovoy@users.noreply.github.com>
2022-09-04 13:51:09 +0800

    Update Global.py

--- action/Global.py
+++ action/Global.py
@@ -1,9 +1,10 @@
 import requests
 
 rawGlobal = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Global/Global.list").text
+rawTelegram = requests.get("https://raw.githubusercontent.com/VirgilClyne/VirgilClyne/main/modules/ASN/ASN.Telegram.list").text
 
 result = list()
-for rawresult in [rawGlobal]:
+for rawresult in [rawGlobal , rawTelegram]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 
