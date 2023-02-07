7e5665a74dc18c3305c82e48c53b8a0e0b3fe877
Troy <105072381+Trovoy@users.noreply.github.com>
2023-02-07 13:04:48 +0800

    更新 Direct.py

--- action/Direct.py
+++ action/Direct.py
@@ -2,9 +2,10 @@ import requests
 
 rawWeChat = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/WeChat/WeChat.list").text
 rawTesla = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Tesla/Tesla.list").text
+rawXiaoHongShu = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/XiaoHongShu/XiaoHongShu.list").text
 
 result = list()
-for rawresult in [rawWeChat , rawTesla]:
+for rawresult in [rawWeChat , rawTesla , rawXiaoHongShu]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 
