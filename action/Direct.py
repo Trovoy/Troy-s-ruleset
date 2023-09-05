d491b31435c6a827804722c7cb1097fe757730b7
Troy <105072381+Trovoy@users.noreply.github.com>
2023-09-05 15:44:48 +0800

    Update Direct.py

--- action/Direct.py
+++ action/Direct.py
@@ -5,9 +5,11 @@ rawTesla = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule
 rawXiaoHongShu = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/XiaoHongShu/XiaoHongShu.list").text
 rawApple = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Apple/Apple.list").text
 rawXianYu = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/XianYu/XianYu.list").text
+rawdirect1 = requests.get("https://raw.githubusercontent.com/dler-io/Rules/main/Surge/Surge%203/Provider/Domestic.list").text
+rawdirect2 = requests.get("https://raw.githubusercontent.com/dler-io/Rules/main/Surge/Surge%203/Provider/Domestic%20IPs.list").text
 
 result = list()
-for rawresult in [rawWeChat , rawTesla , rawXiaoHongShu , rawApple ,rawXianYu]:
+for rawresult in [rawWeChat , rawTesla , rawXiaoHongShu , rawApple ,rawXianYu ,rawdirect1 ,rawdirect2]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 
